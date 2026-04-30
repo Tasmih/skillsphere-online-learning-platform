6a24bbde9f5f727a8db96c224b1a29b5a46f2a06
Tasmih <tasmih.mow20@gmail.com>
2026-05-01 01:26:29 +0600

    Implement Home page hero section UI  and add Popular Courses section with dynamic card layout

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,14 +1,18 @@
 "use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { Menu } from "lucide-react";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <header className="border-b bg-white sticky top-0 z-50">
+    <div className="border-b bg-white sticky top-0 z-50">
       <nav className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
 
-        
+        {/* Logo */}
         <div className="flex items-center gap-2">
           <Image
             src="/assets/logo.png"
@@ -23,49 +27,56 @@ const Navbar = () => {
           </h3>
         </div>
 
-        
+
         <ul className="hidden md:flex items-center gap-6 text-sm font-medium">
-          <li>
-            <Link href="/" className="hover:text-blue-600 transition">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/courses" className="hover:text-blue-600 transition">
-              Courses
-            </Link>
-          </li>
-          <li>
-            <Link href="/profile" className="hover:text-blue-600 transition">
-              My Profile
-            </Link>
-          </li>
+          <li><Link href={"/"}>Home</Link></li>
+          <li><Link href={"/all-courses"}>Courses</Link></li>
+          <li><Link href={"/profile"}>My Profile</Link></li>
         </ul>
 
         
         <div className="hidden md:flex items-center gap-3">
           <Link
             href="/login"
-            className="text-sm border px-4 py-1.5 rounded-md hover:bg-gray-100 transition"
+            className="text-sm border px-4 py-1.5 rounded-md hover:bg-gray-100"
           >
             Login
           </Link>
 
           <Link
-            href="/signup"
-            className="text-sm bg-orange-500 text-white px-4 py-1.5 rounded-md hover:bg-orange-600 transition"
+            href="/register"
+            className="text-sm bg-orange-500 text-white px-4 py-1.5 rounded-md hover:bg-orange-600"
           >
-            Sign Up
+            Register
           </Link>
         </div>
 
-        {/* for mobile  */}
-        <button className="md:hidden">
-          <Menu className="w-6 h-6 text-gray-700" />
+        {/* mobile  btn */}
+        <button className="md:hidden" onClick={() => setOpen(!open)}>
+          <Menu className="w-6 h-6" />
         </button>
-
       </nav>
-    </header>
+
+      {/* Mobile Menu */}
+      {open && (
+        <div className="md:hidden px-4 pb-4 flex flex-col gap-3">
+          <Link href="/" onClick={() => setOpen(false)}>Home</Link>
+          <Link href={"/all-courses"} onClick={() => setOpen(false)}>Courses</Link>
+          <Link href={"/profile"} onClick={() => setOpen(false)}>My Profile</Link>
+
+          <Link href={"/login"} className="border px-3 py-1 rounded">
+            Login
+          </Link>
+
+          <Link
+            href={"/register"}
+            className="bg-orange-500 text-white px-3 py-1 rounded"
+          >
+            Register
+          </Link>
+        </div>
+      )}
+    </div>
   );
 };
 
